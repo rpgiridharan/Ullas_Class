44a4b81355b51db6a58f23b1f3cefc038996efe0
rpgiridharan <rpgiridharan@gmail.com>
2020-03-22 11:32:55 +0530

    commit

--- Day9/4_foo.py
+++ Day9/4_foo.py
@@ -9,8 +9,8 @@ def multiplier(x):
 twice = multiplier(2)
 print(twice(5))
 print(twice(10))
-
 thrice = multiplier(3)
+
 # closure remembers the parameter x,
 # hence, multiplier(3) returns a different function object than multiplier(2)
 print(thrice(4))
